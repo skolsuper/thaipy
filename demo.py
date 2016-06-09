2037d65d1a51324d95b58a2154c836ee53b4b8a4
James Keys <skolsuper@gmail.com>
2016-06-09 17:30:33 +0700

    Add crude demonstration server. (cherry picked from commit 1fd3ca9)

---
+++ demo.py
@@ -0,0 +1,30 @@
+import asyncio
+
+import time
+from aiohttp import web
+from aiohttp.web import Response
+
+request_counter = 0
+
+
+async def fast_handler(request):
+    global request_counter
+    request_counter += 1
+    return Response(text='Hello visitor number {}'.format(request_counter))
+
+
+async def slow_handler(request):
+    time.sleep(10)
+    return Response(text='Heeeeellllllllllllooooooooooooo')
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/slow', slow_handler)
+    app.router.add_route('GET', '/fast', fast_handler)
+
+    web.run_app(app, port=8888)
+
+if __name__ == '__main__':
+    main()
